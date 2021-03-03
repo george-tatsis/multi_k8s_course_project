78132f5ebbb73995b2e3f93468c67b3c6f860ff5
george-tatsis <tat.george90@gmail.com>
2021-03-04 01:42:38 +0200

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t georgetatsis/multi-client:latest -t georgetatsis/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t georgetatsis/multi-server:latest -t georgetatsis/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t georgetatsis/multi-worker:latest -t georgetatsis/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+
+docker push georgetatsis/multi-client:latest
+docker push georgetatsis/multi-server:latest
+docker push georgetatsis/multi-worker:latest
+
+docker push georgetatsis/multi-client:GIT_SHA
+docker push georgetatsis/multi-server:GIT_SHA
+docker push georgetatsis/multi-worker:GIT_SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=georgetatsis/multi-server:$GIT_SHA
+kubectl set image deployments/client-deployment server=georgetatsis/multi-client:$GIT_SHA
+kubectl set image deployments/worker-deployment server=georgetatsis/multi-worker:$GIT_SHA
